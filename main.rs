3a6abd62a14712d6abe19f4183e53ef325fbee3c
RachitVargas <65442367+RachitVargas@users.noreply.github.com>
2020-10-27 00:22:42 -0600

    Se aplicaron cambios

--- main.rs
+++ main.rs
@@ -6,9 +6,9 @@ fn main() {
     let mut message  = String::new();
     println!("Hi, enter the message");
     match io::stdin().read_line(&mut message){
-       Ok(_) => {
-           println!("You message is: {}", message)
-       }
+        Ok(_) => {
+            println!("Your message is: {}", message)
+        }
         _ => {}
     }
 
@@ -16,7 +16,7 @@ fn main() {
     println!("Now, enter the key");
     match io::stdin().read_line(&mut key){
         Ok(_) => {
-            println!("You key is: {}", key)
+            println!("Your key is: {}", key)
         }
         _ => {}
     }
@@ -29,51 +29,51 @@ fn main() {
         println!("Again, enter the key");
         match io::stdin().read_line(&mut key_two){
             Ok(_) => {
-                println!("You key is: {}", key_two)
+                println!("Your key is: {}", key_two)
             }
             _ => {}
         }
 
-        let message_decrypt = decrypt(String::from(ciphertext), String::from(key_two));
-        println!("This is the mesagge: {}", message_decrypt);
+        if (key_two.len() == ciphertext.len()) {
+            let message_decrypt = decrypt(String::from(ciphertext),
+                                          String::from(key_two));
+            println!("This is the mesagge: {}", message_decrypt);
+        }else{
+            println!("sorry, the length of the key and the previous must be same")
+        }
 
     }else{
-       println!("sorry, the length of the key and the message must be the same length")
+        println!("sorry, the length of the key and the message must be the same length")
     }
 
-    println!("Thank you for use the program!")
+    println!("Thank you for use the One Time Pad!")
 }
 
 fn encrypt(message: String, key: String) -> String {
 
     let mut ciphertext = String::new();
+    let message_char: Vec<char> = message.chars().collect();
+    let key_char: Vec<char> = key.chars().collect();
 
-    let message_characters: Vec<char> = message.chars().collect();
-    let key_characters: Vec<char> = key.chars().collect();
-
-    let mut xored_value: u32;
+    let mut xor_aux: u32;
 
     for i in 0 .. key.chars().count() {
-        xored_value = u32::from(message_characters[i]) ^ u32::from(key_characters[i]);
-
-        let option = std::char::from_u32(xored_value);
-        ciphertext.push(*option.as_ref().unwrap());
+        xor_aux = u32::from(message_char[i]) ^ u32::from(key_char[i]);
+        let help_to_push = std::char::from_u32(xor_aux);
+        ciphertext.push(*help_to_push.as_ref().unwrap());
     }
-
     return String::from(ciphertext.as_str());
-
 }
 
 fn decrypt (ciphertext:String, key_two: String) -> String{
 
     let mut message = String::new();
-    let ciphertext_characters: Vec<char> = ciphertext.chars().collect();
-    let key_characters: Vec<char> = key_two.chars().collect();
+    let ciphertext_char: Vec<char> = ciphertext.chars().collect();
+    let key_char: Vec<char> = key_two.chars().collect();
     let mut xor_aux: u32;
 
     for i in 0 .. key_two.chars().count() {
-        xor_aux = u32::from(ciphertext_characters[i]) ^ u32::from(key_characters[i]);
-
+        xor_aux = u32::from(ciphertext_char[i]) ^ u32::from(key_char[i]);
         let help_to_push = std::char::from_u32(xor_aux);
         message.push(*help_to_push.as_ref().unwrap());
     }
