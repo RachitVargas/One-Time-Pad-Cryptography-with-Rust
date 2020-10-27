b804e878a3af6b4d19b0bf7fc2d16e77b524ad6c
RachitVargas <65442367+RachitVargas@users.noreply.github.com>
2020-10-26 19:35:12 -0600

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,26 +1,52 @@
 use ferris_says::say;
-use std::io::{stdout,  BufWriter};
+use std::io::{stdout, BufWriter, Bytes, Error};
+use std::io;
+use std::str::from_utf8;
+use std::ops::{BitXor, BitXorAssign};
 
 fn main() {
 
-    let message = "s";
-    let key = "s";
+    let mut message  = String::new();
 
-    let ciphertext =  encrypt(String::from(message), String::from(key));
-    println!("This is the ciphertext: {}",ciphertext);
+    println!("Hi, enter the message");
+    match io::stdin().read_line(&mut message){
+       Ok(_) => {
+           println!("You message is: {}", message)
+       }
+        _ => {}
+    }
 
-  // let message_decrypt = decrypt(String::from(ciphertext), String::from(key));
-    //println!("{}", std::str::from_utf8((&message_decrypt).as_ref()).unwrap());
+    let mut key = String::new();
+
+    println!("Now, enter the key");
+    match io::stdin().read_line(&mut key){
+        Ok(_) => {
+            println!("You key is: {}", key)
+        }
+        _ => {}
+    }
+
+    if (message.len() == key.len()){
+        let ciphertext =  encrypt(String::from(message), String::from(key));
+        println!("This is the ciphertext: {}", ciphertext);
+
+        //let message_decrypt = decrypt(String::from(ciphertext), String::from(key));
+        //println!("This is the mesagge: {}", message_decrypt);
+
+    }else{
+       println!("sorry, the length of the key and the message must be the same length")
+    }
+
+    println!("Thank you for use the program!")
 }
 
 fn encrypt(message: String, key: String) -> String {
 
-    let message_bytes = message.as_bytes();
-    let key_bytes = key.as_bytes();
+    let message_bytes= message.as_bytes();
+    let key_bytes= key.as_bytes();
+    let mut ciphertext = ;
 
-    let ciphertext = key_bytes ^ message_bytes;
-
-    return  String::from("d");
+    return String::from(std::str::from_utf8(&ciphertext).unwrap());
 }
 
 //fn decrypt(ciphertext: String, key: String) -> String {
@@ -28,7 +54,5 @@ fn encrypt(message: String, key: String) -> String {
 //      let key_bytes = key.as_bytes();
 //      let message = key_bytes ^ ciphertext;
 //
-  //  return  String::from(message);
+  //  return  String::from(std::str::from_utf8(&message).unwrap());
 //}
-
-//println!("{}", std::str::from_utf8(&key_bytes).unwrap());
